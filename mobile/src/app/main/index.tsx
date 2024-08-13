60f33f76223b7f3cc929b2f564a0f6f9d97eafaf
Douglas Faria <hfsdouglas@gmail.com>
2024-08-12 21:20:36 -0300

    fix: padding bottom navbar

--- mobile/src/app/main/index.tsx
+++ mobile/src/app/main/index.tsx
@@ -1,15 +1,17 @@
-import { View } from "react-native";
+import { Platform, View } from "react-native";
 
 import { TabLoja, TabPedidos, TabPerfil, TabProducts } from "@/components/tab-itens";
 
 import Produtos from "../produtos";
+
+const paddingBottom = Platform.OS === 'ios' ? 'pb-8' : ''
 
 export default function Main() {
     return (
         <View className="flex-1 relative">
             <Produtos />
             
-            <View className="absolute bottom-0 w-full h-auto bg-slate-500/15 flex-row justify-around p-4">
+            <View className={`absolute bottom-0 w-full h-auto bg-slate-500/15 flex-row justify-around p-4 ${paddingBottom}`}>
                 <TabLoja />
                 <TabProducts />
                 <TabPedidos />
