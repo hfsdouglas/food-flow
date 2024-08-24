51df62c852d5b1bb55cb3350506a2b8db6a4424f
Douglas Faria <hfsdouglas@gmail.com>
2024-08-23 21:16:35 -0300

    feat: product data for loading the page

---
+++ src/utils/products.ts
@@ -0,0 +1,81 @@
+export const MENU = [
+  {
+    title: "Brownie",
+    data: [
+      {
+        id: "1",
+        name: "Brownie PERFEITO - recheado com doce de leite vegano",
+        description: "Chocolate 70% cacau adoçado com açúcar de coco (massa de cacau...",
+        value: 52.00,
+        image: "http://localhost"
+      },
+      {
+        id: "2",
+        name: "Brownie CROCANTE - com pedaços de nozes",
+        description: "Chocolate 70% cacau adoçado com melado de cana, com pedaços de nozes para um toque crocante.",
+        value: 54.00,
+        image: "http://localhost"
+      }
+    ]
+  },
+  {
+    title: "Cookies",
+    data: [
+      {
+        id: "3",
+        name: "Cookies VEGANOS - recheados com chocolate amargo",
+        description: "Deliciosos cookies veganos feitos com farinha de amêndoas e recheados com chocolate amargo 70% cacau.",
+        value: 38.00,
+        image: "http://localhost"
+      }
+    ]
+  },
+  {
+    title: "Bebidas",
+    data: [
+      {
+        id: "4",
+        name: "Smoothie de Frutas Vermelhas - com leite de amêndoas",
+        description: "Smoothie refrescante de frutas vermelhas, adoçado naturalmente e feito com leite de amêndoas.",
+        value: 28.00,
+        image: "http://localhost"
+      }
+    ]
+  },
+  {
+    title: "Pães",
+    data: [
+      {
+        id: "5",
+        name: "Pão Integral Artesanal - com sementes de girassol",
+        description: "Pão integral artesanal, feito com farinha orgânica e enriquecido com sementes de girassol.",
+        value: 25.00,
+        image: "http://localhost"
+      }
+    ]
+  },
+  {
+    title: "Donuts",
+    data: [
+      {
+        id: "6",
+        name: "Donuts VEGANOS - com cobertura de chocolate",
+        description: "Donuts veganos macios, cobertos com chocolate 70% cacau e decorados com coco ralado.",
+        value: 32.00,
+        image: "http://localhost"
+      }
+    ]
+  },
+  {
+    title: "Doces",
+    data: [
+      {
+        id: "7",
+        name: "Trufas de Chocolate - com recheio de maracujá",
+        description: "Trufas artesanais de chocolate amargo 70%, com recheio cremoso de maracujá.",
+        value: 48.00,
+        image: "http://localhost"
+      }
+    ]
+  }
+]
