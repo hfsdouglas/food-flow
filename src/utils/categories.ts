eb9f7f6cb54ccb8935ffeb32f8646c465f9f07e5
Douglas Faria <hfsdouglas@gmail.com>
2024-08-23 22:15:38 -0300

    feat: categories data for loading flatlist

---
+++ src/utils/categories.ts
@@ -0,0 +1,8 @@
+export const CATEGORIES = [
+    { title: "BROWNIE" },
+    { title: "COOKIES" },
+    { title: "BEBIDAS" },
+    { title: "PÃES" },
+    { title: "DONUTS" },
+    { title: "DOCES" },
+]
