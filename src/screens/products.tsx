b785eff6950d3daf139d292794b48a355add4fd0
Douglas Faria <hfsdouglas@gmail.com>
2024-08-24 00:31:57 -0300

    feat: hook provider on product screen

--- src/screens/products.tsx
+++ src/screens/products.tsx
@@ -1,38 +1,21 @@
-import { SectionList, Text, View } from 'react-native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 type RootStackParamList = {
   Detalhes: undefined;
 }
 
-type ProductsProps = {
+export type ProductsProps = {
   navigation: NativeStackNavigationProp<RootStackParamList>
 }
 
-import { ProductsHeader } from '@/components/products-header';
-import { ProductCard } from '@/components/product-card';
+import { ProductBody } from '@/components/product-body';
 
-import { MENU } from '@/utils/products';
+import { SearchModeProvider } from '@/hooks/useSeachMode';
 
-export default function Products({ navigation }: ProductsProps) {
+export default function Products({ navigation }: ProductsProps) { 
   return (
-    <View className="flex-1">
-      <ProductsHeader />
-      
-      <SectionList 
-        sections={MENU} 
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <ProductCard onPress={() => navigation.navigate('Detalhes')} product={item}/> 
-        )}
-        renderSectionHeader={({ section: { title } }) => (
-          <Text className="text-xl text-slate-400 font-bold mt-5">{title}</Text>
-        )}
-        className="flex-1 p-5"
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 100 }}
-        stickySectionHeadersEnabled={false}
-      />
-    </View>
+    <SearchModeProvider>
+      <ProductBody navigation={navigation} />
+    </SearchModeProvider>
   );
 }
