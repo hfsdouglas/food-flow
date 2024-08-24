927f685d5000e507893777e0195e5a48168e7b4f
Douglas Faria <hfsdouglas@gmail.com>
2024-08-23 21:17:06 -0300

    feat: preparing product cart to receive data

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -1,30 +1,32 @@
 import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
 
 interface Product {
-    id: number,
-    nome: string, 
-    preco: number, 
-    text: string, 
-    image: string
+    id: string,
+    name: string, 
+    description: string, 
+    value: number
+    image?: string 
 }
 
 interface ProductCardProps extends TouchableOpacityProps {
-    product?: Product
+    product: Product
 }
 
-export default function ProductCard({ product, ...rest }: ProductCardProps) {
+export function ProductCard({ product, ...rest }: ProductCardProps) {
+    const { name, description, value, image } = product
+
     return (
         <TouchableOpacity 
-            className="h-36 mt-5 p-3 flex-row gap-3 bg-slate-900 rounded-xl"
+            className="h-36 p-3 mt-5 flex-row gap-3 bg-slate-900 rounded-xl"
             activeOpacity={0.9}
             {...rest}
         >
             <View className="w-[102px] h-[102px] bg-red-500 border rounded-lg"></View>
 
             <View className="flex-1 justify-between">
-                <Text className="font-bold text-slate-400 text-md">Brownie PERFEITO - recheado com doce de leite vegano</Text>
-                <Text className="font-regular text-slate-400/80 text-xs">Chocolate 70% cacau adoçado com açúcar de coco (massa de cacau...</Text>
-                <Text className="font-bold uppercase text-lime-400">R$ 52,00</Text>
+                <Text className="font-bold text-slate-400 text-md">{name}</Text>
+                <Text className="font-regular text-slate-400/80 text-xs">{description}</Text>
+                <Text className="font-bold uppercase text-lime-400">{value}</Text>
             </View>
         </TouchableOpacity>
     )
